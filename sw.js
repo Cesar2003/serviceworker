55a0f5f144d9b9d9268bcfbf6d5b6af4a33fea6c
Cesar2003 <jcgarcia-2003@hotmail.com>
2018-08-10 22:21:52 -0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,34 @@
+var cacheName = 'Julio Garcia v1';
+
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll([
+       
+                  '/',
+                  '/index.html',
+                  '/main.js',
+                  '/style.css'
+
+                  ]);
+    })
+  );
+});
+
+
+
+this.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(resp) {
+      return resp || fetch(event.request).then(function(response) {
+        return caches.open('Julio Garcia v1').then(function(cache) {
+          cache.put(event.request, response.clone());
+          return response;
+        });  
+      });
+    })
+  );
+});
